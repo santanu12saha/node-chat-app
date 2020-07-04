376956778bb3e84129e456e0e5cd41298ca8ef76
santanu12saha <saha.santanu0217@gmail.com>
2020-07-04 13:18:04 +0530

    create generate message utility.

---
+++ server/utils/message.test.js
@@ -0,0 +1,13 @@
+const expect = require('expect');
+const { generateMessage } = require('./message');
+
+describe('generateMessage', () => {
+    it('should generate correct message object', () => {
+        var from = 'Tapasmita', 
+        text = 'Some message';
+        var message = generateMessage(from, text);
+
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({from,text});
+    });
+});
